db9e355ad58757020443a77f484f4740637ce1c6
FireFlame <57473470+Valstrude@users.noreply.github.com>
2024-08-02 22:17:02 -0300

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,24 @@
+body {
+    color: white;
+    background: black;
+    margin: 0px;
+}
+
+header {
+    border-bottom: solid 2px rgb(42, 122, 228);
+    padding: 20px;
+    font-size: 32px;
+    color: rgb(42, 122, 228);
+}
+
+section {
+    background: rgb(184,156,213);
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+}
+
+.chamada-texto {
+    margin-right: 5%;
+}
